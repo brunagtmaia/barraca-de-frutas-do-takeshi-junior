619e751e27462f739d6c6b9d6248d744a3272d1f
brunagtmaia <brunagmaia05@gmail.com>
2023-07-31 21:23:40 -0300

    Ambiente

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all', //sempre ter virgula
+  singleQuote: true,
+  printWidth: 100, //larguna que vai ter uma linha, então a partir de 100 caracteres é obrigado a pular para a linha seguinte 
+  tabWidth: 2,//quantidade de espaços que cada tela vai ter 
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+    return(
+        <SafeAreaView>
+            <Text>Testando</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App;
+
